f4eda57d98ac566e13684eefa53d9fd8067e34f7
Radiyah <rsabur58@gmail.com>
2021-02-27 20:32:26 -0500

    commit tree 6cc7137422b31b0f578522a8f2c4af3fb84e5358

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
-# Add your code here
+require 'pry'
+
+class Dog
+    
+    attr_accessor :name, :save
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all.each
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    # binding.pry
+    def self.print_all
+        all.each do |dog|
+            puts dog.name
+        end
+    end
+
+    def save
+        @@all << self
+    end
+
+end
